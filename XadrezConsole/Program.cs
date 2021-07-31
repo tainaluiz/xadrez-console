0d38dabb3cb8995847a2fb43ab00cdc10c1d274e
Tainã Luiz <tluuiiz@gmail.com>
2021-07-31 20:33:10 -0300

    Criação classe Posicao

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using XadrezConsole.Tabuleiro;
 
 namespace XadrezConsole {
     class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        static void Main() {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine(p);
         }
     }
 }
---
+++ XadrezConsole/Tabuleiro/Posicao.cs
@@ -0,0 +1,16 @@
+﻿namespace XadrezConsole.Tabuleiro {
+    class Posicao {
+
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+
+        public override string ToString() {
+            return $"Posicao {Linha}, {Coluna}";
+        }
+    }
+}
